15e3dab093ff96cce4c7b2d5914753dfc7367e18
Andres Zapata <azapata@uc.edu.co>
2021-07-07 22:58:26 -0500

    Fin Seccion 4 fundamento angular

---
+++ Bases-Angular/src/app/heroes/listado/listado.component.ts
@@ -0,0 +1,16 @@
+import { Component} from '@angular/core';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html'
+})
+export class ListadoComponent  {
+
+  heroes: string[]= ['Hulk', 'Ironman','Superman','Thor','Spiderman']
+  heroeBorrado: string = '';
+  borrarHeroe(): void{
+    console.log('Borrando')
+    this.heroeBorrado = this.heroes.pop() || '';
+  }
+
+}
