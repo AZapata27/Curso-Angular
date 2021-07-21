28819f5c9dc748feae964b06fa4bc28624cc9e95
Andres Zapata <azapata@uc.edu.co>
2021-07-21 00:01:33 -0500

    Se agrega subscripcion al api de gif y mostrar los gifs en el componente de resultados y busqueda

--- gifsApp/src/app/gifs/services/gifs.service.ts
+++ gifsApp/src/app/gifs/services/gifs.service.ts
@@ -1,26 +1,36 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class GifsService {
 
+  private apiKeyGifs: string = "TneJmJH4EJpGTFgwmnxeBlaRfhyXoh5T";
   private _historial: string[]=[];
+  public resultados: any[]=[];
 
   get historial(){
 
     return [...this._historial];
   }
+
+  constructor(private http: HttpClient) {}
+
   buscarGifs(query: string){
 
     query = query.trim().toLowerCase();
 
     if( !this._historial.includes(query)){
       this._historial.unshift(query);
+      this._historial= this._historial.splice(0.10);
     }
-    this._historial= this._historial.splice(0.10);
 
-    console.log( this._historial)
+    this.http.get(`https://api.giphy.com/v1/gifs/search?api_key=${this.apiKeyGifs}&q=${query}&limit=10`)
+      .subscribe( (resp:any)=>{
+        console.log(resp.data);
+        this.resultados= resp.data;
+      });
 
   }
 }
