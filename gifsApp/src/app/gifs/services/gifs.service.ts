f33b52eabd65306735877d955da0cf0f112603a9
Andres Zapata <azapata@uc.edu.co>
2021-08-11 20:27:28 -0500

    Terminacion de appgifs para maximar eficiencia de carga de api y constantes de servicios

--- gifsApp/src/app/gifs/services/gifs.service.ts
+++ gifsApp/src/app/gifs/services/gifs.service.ts
@@ -1,21 +1,26 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpParams} from "@angular/common/http";
+import {Gif, SearchGifsResponse} from "../interface/gifs.interface";
 
 @Injectable({
   providedIn: 'root'
 })
 export class GifsService {
 
-  private apiKeyGifs: string = "TneJmJH4EJpGTFgwmnxeBlaRfhyXoh5T";
+  private apiKeyGifs: string = 'TneJmJH4EJpGTFgwmnxeBlaRfhyXoh5T';
+  private servicioUrl: string = 'https://api.giphy.com/v1/gifs'
   private _historial: string[]=[];
-  public resultados: any[]=[];
+  public resultados: Gif[]=[];
 
   get historial(){
 
     return [...this._historial];
   }
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+    this._historial = JSON.parse(localStorage.getItem('historial')! )  || [];
+    this.resultados = JSON.parse(localStorage.getItem('ultimosResultados')! )  || [];
+  }
 
   buscarGifs(query: string){
 
@@ -24,12 +29,21 @@ export class GifsService {
     if( !this._historial.includes(query)){
       this._historial.unshift(query);
       this._historial= this._historial.splice(0.10);
+      localStorage.setItem('historial',JSON.stringify(this._historial));
+
     }
 
-    this.http.get(`https://api.giphy.com/v1/gifs/search?api_key=${this.apiKeyGifs}&q=${query}&limit=10`)
-      .subscribe( (resp:any)=>{
+    const params = new HttpParams()
+      .set('api_key', this.apiKeyGifs)
+      .set('limit','10')
+      .set('q',query)
+    ;
+
+    this.http.get<SearchGifsResponse>(`${this.servicioUrl}/search`, {params })
+      .subscribe( (resp)=>{
         console.log(resp.data);
         this.resultados= resp.data;
+        localStorage.setItem("ultimosResultados", JSON.stringify(this.resultados));
       });
 
   }
