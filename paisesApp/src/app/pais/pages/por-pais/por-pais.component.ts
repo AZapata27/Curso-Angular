e0cd1a6cf05baeeb2580317fc4246e7e077c71f6
azapatar91013 <azapatar91013@desa.banrep.gov.co>
2022-04-10 12:46:45 -0500

    mejora estructura

--- paisesApp/src/app/pais/pages/por-pais/por-pais.component.ts
+++ paisesApp/src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,27 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import {Component} from '@angular/core';
 import {PaisService} from "../../services/pais.service";
 
 @Component({
-  selector: 'app-por-pais',
-  templateUrl: './por-pais.component.html',
-  styles: [
-  ]
+  selector: 'app-por-pais', templateUrl: './por-pais.component.html', styles: []
 })
-export class PorPaisComponent{
-
-  constructor(private paisService: PaisService) { }
+export class PorPaisComponent {
 
   termino: string = "Hola";
   isError: boolean = false;
 
-  buscar(){
-    this.isError =false
+  constructor(private paisService: PaisService) {
+  }
+
+  buscar() {
+    this.isError = false
     console.log(this.termino);
     this.paisService.buscarPais(this.termino)
-      .subscribe( resp =>{
+      .subscribe(resp => {
         console.log(resp);
       }, error => {
-        this.isError =true;
+        this.isError = true;
       });
   }
 }
